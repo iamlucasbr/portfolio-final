ad70f7f90251bfa0f2625c321453a357e1cd52a2
Lucas Oliveira <contatolucasfernandesbr@gmail.com>
2025-10-27 19:26:59 -0300

    Fix(all): Restaura config local e troca a action de FTP

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Esta linha é necessária para as Imagens do Next.js
-  // funcionarem no site estático final (no deploy).
-  // Não vai quebrar seu localhost.
+ 
   images: {
     unoptimized: true,
   },
