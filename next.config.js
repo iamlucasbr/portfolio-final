a5971911c6f70c69e2534a9a3ce1f365e19e7612
Lucas Oliveira <contatolucasfernandesbr@gmail.com>
2025-10-27 20:32:21 -0300

    Fix(all): Atualiza config e troca action de FTP (via VSCode)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  // Esta linha é necessária para as Imagens do Next.js
+  // funcionarem no site estático final (no deploy).
+  // Não vai quebrar seu localhost.
   images: {
     unoptimized: true,
   },
