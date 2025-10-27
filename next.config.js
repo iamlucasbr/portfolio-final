24d17837f9985a6ba8d30323b549743ed02f0b46
Lucas Oliveira <contatolucasfernandesbr@gmail.com>
2025-10-27 16:41:57 -0300

    Fix(deploy): Força 'next export' no build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-// Detecta se estamos rodando o build de produção (no GitHub Actions)
-const isProductionBuild = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  // SÓ VAI ATIVAR O 'export' DURANTE O BUILD DE PRODUÇÃO (no GitHub)
-  // Isso corrige o 'npm run dev' (localhost)
-  output: isProductionBuild ? 'export' : undefined,
-
-  // ESSA LINHA É NECESSÁRIA QUANDO SE USA 'output: "export"'
-  // para as imagens do Next.js funcionarem no site final
+  // Isso é necessário para as Imagens do Next.js 
+  // funcionarem no site estático final.
   images: {
     unoptimized: true,
   },
